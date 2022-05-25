9234a1eb20ccf49e3b677ff7f9965a8e9eaa5782
Akhil V Pillai <cs30sp22pj@ieng6-201.ucsd.edu>
2022-05-25 11:42:19 -0700

    Added PA5

---
+++ PA5/encrypter.h
@@ -0,0 +1,17 @@
+// versions 1.0 05/22/2022
+#ifndef _cipher_h
+#define _cipher_h 1
+#define ENCRYPT_MODE	1
+#define DECRYPT_MODE	0
+#define	EXIT_OK		0
+#define	EXIT_FAIL	-1
+#ifndef __ASSEMBLER__
+int setup(int, char **, int *, FILE **, FILE **, FILE **);
+int rdbuf(char *, char *, FILE *, FILE *, int);
+int encrypt(char *, char *, int);
+int decrypt(char *, char *, int);
+int wrbuf(char *, int, FILE *);
+int cleanup(int);
+void errmsg(char *);
+#endif
+#endif
